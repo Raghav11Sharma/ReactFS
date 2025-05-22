bb1c35109c63b40abcb2e7fb1d01df1b07bd64b4
RaghavSharma <RaghavSharma@EPINGUGW0487>
2025-05-23 00:48:57 +0530

    tbh

--- ReactDotnetFS/src/App.tsx
+++ ReactDotnetFS/src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import FirstComponent from './Components/FirstComponent';
 
 function App() {
- 
-
   return (
-    <>
-    Hello Bhadwe
-    </>
-   
+    <div className='container'> 
+      <FirstComponent subtitle=' Hellor Mr Raghav Instagram King'/> 
+    </div>
+  
   );
 }
 
-export default App
+export default App;
--- ReactDotnetFS/src/Components/FirstComponent.tsx
+++ ReactDotnetFS/src/Components/FirstComponent.tsx
@@ -1,11 +1,25 @@
-function FirtComponent()
-{
-    const data = () =>{
+import logo from './OIP.jpg';
+import './FirstComponent.css';
 
-    
-    return 
-    <>
-    <div> Hello Raghav</div>
-    </>
-    };
+type args= {
+    subtitle:string
 }
+const FirstComponent = ({subtitle} : args) =>{
+    return (
+         
+        <header className="row mb-4">
+            <div className='col-2'>
+
+                <img src={logo}
+                className="logo"
+                alt="logo"
+                />
+            </div>
+            <div className='col-10 mt-5 subtitle'>{subtitle}</div>
+        </header>
+    );
+}
+
+
+
+export default FirstComponent;
